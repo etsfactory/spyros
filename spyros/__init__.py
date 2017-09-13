c0f6992e8685a7ac91b8508f1a785eef688d2486
ÓscarGF <masdeseiscaracteres@gmail.com>
2017-09-13 12:36:04 +0200

    Update __init__.py

--- spyros/__init__.py
+++ spyros/__init__.py
@@ -10,7 +10,8 @@ nanmin = bn.nanmin
 nanargmax = bn.nanargmax
 nanargmin = bn.nanargmin
 
-# Others require custom implementations
-# nancummax
-# nancummin
+# Others, not available out there, require custom implementations
+# Take a look at https://docs.scipy.org/doc/numpy-1.13.0/reference/ufuncs.html#methods
+# nancummax (np.maximum.accumulate + masking?)
+# nancummin (np.minimum.accumulate + masking?)
 
