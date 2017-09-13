f07d7d0bac2a5ab836a6d758b08ab5deec5a3632
ÓscarGF <masdeseiscaracteres@gmail.com>
2017-09-13 15:22:30 +0200

    Update __init__.py

--- spyros/__init__.py
+++ spyros/__init__.py
@@ -1,18 +1,46 @@
+import numpy as np
 import bottleneck as bn
 
-# First import some bottleneck functions
-# See http://berkeleyanalytics.com/bottleneck/reference.html# for more
-nanmean = bn.nanmean
-nanstd = bn.nanstd
-nansum = bn.nansum
-nanmax = bn.nanmax
-nanmin = bn.nanmin
-nanargmax = bn.nanargmax
-nanargmin = bn.nanargmin
-
-# Others, not available out there, require custom implementations
+# Function are basically picked from distinct sources
+# 1. Bottleneck functions
+# Reference: http://berkeleyanalytics.com/bottleneck/reference.html
+# 2. Numpy functions
+# Reference: https://docs.scipy.org/doc/numpy/reference/routines.math.html
+# 3. Those that are not available out there, require custom implementations:
 # Take a look at https://docs.scipy.org/doc/numpy-1.13.0/reference/ufuncs.html#methods for ideas using numpy ufunc's
-def nancummax(arr, axis=0):
+
+## Function implementation starts here
+# Reduce functions
+mean = bn.nanmean
+std = bn.nanstd
+var = bn.nanvar
+sum = bn.nansum
+max = bn.nanmax
+min = bn.nanmin
+argmax = bn.nanargmax
+argmin = bn.nanargmin
+median = bn.nanmedian
+ss = bn.ss
+anynan = bn.anynan
+allnan = bn.allnan
+	
+# Moving window functions
+movsum = bn.move_sum
+movmean = bn.move_mean
+movstd = bn.move_std
+movvar = bn.move_var
+movmin = bn.move_min, 
+movmax = bn.move_max
+movargmin = bn.move_argmin
+movargmax = bn.move_argmax
+movmedian = bn.move_median
+movrank = bn.move_rank
+
+# Expanding window functions
+cumsum = np.nancumsum
+cumprod = np.nancumprod
+
+def cummax(arr, axis=0):
 	out = arr.copy()
 	mask = np.isnan(arr)
 	out[mask] = -np.Inf
@@ -20,7 +48,7 @@ def nancummax(arr, axis=0):
 	out[out==-np.Inf]=np.nan
 	return out
 
-def nancummin(arr, axis=0):
+def cummin(arr, axis=0):
 	out = arr.copy()
 	mask = np.isnan(arr)
 	out[mask] = np.Inf
