fc01242603e437c0e4d06d19b3f21df79ff942b6
ÓscarGF <masdeseiscaracteres@gmail.com>
2017-09-13 12:29:15 +0200

    Create __init__.py

---
+++ spyros/__init__.py
@@ -0,0 +1,16 @@
+import bottleneck as bn
+
+# First import some bottleneck functions
+# See http://berkeleyanalytics.com/bottleneck/reference.html# for more
+nanmean = bn.nanmean
+nanstd = bn.nanstd
+nansum = bn.nansum
+nanmax = bn.nanmax
+nanmin = bn.nanmin
+nanargmax = bn.nanargmax
+nanargmin = bn.nanargmin
+
+# Others require custom implementations
+# nancummax
+# nancummin
+
