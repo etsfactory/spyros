e6b522cbd8add5b7c5c213105690fb9653dfbfd3
ÓscarGF <masdeseiscaracteres@gmail.com>
2017-10-22 12:48:02 +0200

    Update __init__.py

--- spyros/__init__.py
+++ spyros/__init__.py
@@ -1,4 +1,12 @@
-"""Functions in this module are basically picked from distinct sources:
+"""Functions in this package can be of different types:
+
+Function types
+--------------
+- slow: Slow but correct implementations for testing purposes.
+- pure_python: highly portable fast implementations not requiring any other than bottleneck, numpy, pandas or scipy
+- numba: fast implementations requiring numba
+- cython: fast implementations requiring cythonization
+- c: fast implementations in requiring C source compilation
 
 References
 ----------
@@ -15,95 +23,3 @@ Take a look at
 https://docs.scipy.org/doc/numpy-1.13.0/reference/ufuncs.html#methods for ideas
 using numpy ufunc's.
 """
-
-import numpy as np
-import bottleneck as bn  # requires bottleneck >= 1.2.0
-import pandas.core.nanops as nanops
-
-## Function implementation starts here
-# Reducing functions
-mean = bn.nanmean
-std = bn.nanstd
-var = bn.nanvar
-sum = bn.nansum
-prod = nanops.nanprod
-max = bn.nanmax
-min = bn.nanmin
-argmax = bn.nanargmax
-argmin = bn.nanargmin
-median = bn.nanmedian
-ss = bn.ss
-anynan = bn.anynan
-allnan = bn.allnan
-sem = nanops.nansem
-skew = nanops.nanskew
-kurt = nanops.nankurt
-percentile = np.nanpercentile
-
-# Reducing two-argument functions
-cov = nanops.nancov
-corr = nanops.nancorr
-
-# Element-wise functions
-gt = nanops.nangt
-ge = nanops.nange
-lt = nanops.nanlt
-le = nanops.nanle
-eq = nanops.naneq
-ne = nanops.nanne
-
-# Moving window functions
-movsum = bn.move_sum
-movmean = bn.move_mean
-movstd = bn.move_std
-movvar = bn.move_var
-movmin = bn.move_min,
-movmax = bn.move_max
-movargmin = bn.move_argmin
-movargmax = bn.move_argmax
-movmedian = bn.move_median
-movrank = bn.move_rank
-
-# Expanding window functions
-cumsum = np.nancumsum
-cumprod = np.nancumprod
-
-
-def cummax(arr, axis=0):
-    """Cumulative max (in an expanding window).
-
-    Parameters
-    ----------
-    arr : ND-array
-    axis : int
-
-    Returns
-    -------
-    cummax : ND-array
-    """
-    out = arr.copy()
-    mask = np.isnan(arr)
-    out[mask] = -np.Inf
-    np.maximum.accumulate(out, axis=axis, out=out)
-    out[out == -np.Inf] = np.nan
-    return out
-
-
-def cummin(arr, axis=0):
-    """Cumulative min (in an expanding window).
-
-    Parameters
-    ----------
-    arr : ND-array
-    axis : int
-
-    Returns
-    -------
-    cummin : ND-array
-    """
-    out = arr.copy()
-    mask = np.isnan(arr)
-    out[mask] = np.Inf
-    np.minimum.accumulate(out, axis=axis, out=out)
-    out[out == np.Inf] = np.nan
-    return out
