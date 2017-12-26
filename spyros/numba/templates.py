b8ba22f7f24ef7f0fe2f3e9c01393843a062340c
ÓscarGF <masdeseiscaracteres@gmail.com>
2017-12-26 23:50:57 +0100

    Update templates.py

--- spyros/numba/templates.py
+++ spyros/numba/templates.py
@@ -65,12 +65,34 @@ def foo(arr, axis=0):
     return out
 
 
-# To-Do: write cum_returns function
-# Write a finite state machine:
-# for every i, out[i-1] is filled with
-# - np.nan if the first non-nan value has not been found yet
-# - 0 if out[i] is the first non-nan value
-# - cum return ...
+@guvectorize(NUMBA_SIGNATURE_LIST, RESIZE_NUMPY_SIGNATURE, nopython=False)
+def _cum_returns(a, out):
+
+    for i in range(len(a)):
+        if not np.isnan(a[i]):
+            break
+        out[i] = np.nan
+
+    crp1 = 1  # cumulative return plus 1
+    nxt_out = 0  # crp1 - 1
+    for j in range(i, len(a)):
+        out[j] = nxt_out
+        if not np.isnan(a[j]):
+            crp1 *= 1 + a[j]
+            nxt_out = crp1 - 1
+        else:
+            nxt_out = np.nan
+
+    out[len(a)] = nxt_out
+
+
+def cum_returns(arr):  # axis = 0 only
+    arr = arr.T
+    new_shape = list(arr.shape)
+    new_shape[-1] += 1  # alter dimensions however you want
+    out = np.empty(new_shape)  # pre-allocate output array
+    _cum_returns(arr, out)  # fill output array according to computation
+    return out.T
 
 
 # Rolling (moving window)
@@ -95,3 +117,8 @@ if __name__ == "__main__":
     print(out)
     out = foo(A, axis=0)
     print(out)
+    
+    a = np.array([np.nan, 0.01, np.nan, np.nan, 0.02, -0.02, -0.01, np.nan, 0.01])
+    out = np.empty(a.shape[0]+1)
+    _cum_returns(a, out)
+    print(np.round(out, 2))
